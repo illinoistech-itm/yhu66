005b12bb681972872db638e4576a1a5ec160368f
yixin <yhu66@hawk.iit.edu>
2021-03-15 01:29:35 -0500

    pull from team

---
+++ sprint-02/code/db-samples/create-database.sql
@@ -0,0 +1,7 @@
+--  This command will create a database named comments
+-- Test DB
+CREATE DATABASE posts DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+-- Website DB
+CREATE DATABASE bugville DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
