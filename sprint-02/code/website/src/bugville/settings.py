c9ba310bde68b26bb08214b991be0bea7ef1a32e
yixin <yhu66@hawk.iit.edu>
2021-03-16 00:42:12 -0500

    lh

--- sprint-02/code/website/src/bugville/settings.py
+++ sprint-02/code/website/src/bugville/settings.py
@@ -25,7 +25,7 @@ with open('/etc/secret_key.txt') as f:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
