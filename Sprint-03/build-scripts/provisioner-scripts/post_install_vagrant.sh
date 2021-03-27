07262fbc6a1b322a4f458058d8087d58edf4768f
yixin <yhu66@hawk.iit.edu>
2021-03-27 00:07:22 -0500

    change scripts

--- Sprint-03/build-scripts/provisioner-scripts/post_install_vagrant.sh
+++ Sprint-03/build-scripts/provisioner-scripts/post_install_vagrant.sh
@@ -9,17 +9,36 @@ echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
 sudo groupadd admin
 sudo usermod -a -G admin vagrant
 
-# Installing Vagrant keys
+# Installing vagrant keys
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-sudo mkdir -p /home/vagrant/.ssh
+#sudo mkdir -p /home/vagrant/.ssh
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
 cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 echo "All Done!"
 
+
+
 ##################################################
 # Add User customizations below here
 ##################################################
 
+sudo apt-get update
+sudo apt-get install -y nginx fail2ban git
+
+## enable firewall
+ufw --force enable
+ufw allow proto tcp to 0.0.0.0/0 port 22
+ufw allow proto tcp to 0.0.0.0/0 port 80
+ufw allow proto tcp to 0.0.0.0/0 port 443
+ufw allow proto tcp to 0.0.0.0/0 port 3000
+
+# set the /etc/hosts file to match hostname
+echo "192.168.33.33      frontend    frontend.example"    | sudo tee -a /etc/hosts
+echo "192.168.33.34      db    db.example"    | sudo tee -a /etc/hosts
+
+# Set hostname
+sudo hostnamectl set-hostname frontend
 
 
+####Install all your frameworks
